3662a362621b02b198c9da6215c3ff4c74e76adf
39mwan <soniaz1998@gmail.com>
2022-03-10 12:32:20 +0100

    added prettier

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-  roots: ['./src'],
-  // setupFiles: ["./src/setupTests.ts"],
-  setupFilesAfterEnv: ['./src/setupTests.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.svg$': './jest.file.js'
-  },
-  testMatch: [
-    './src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{spec,test}.{js,jsx,ts,tsx}'
-  ],
-  moduleNameMapper: {
-    '\\.(css|sass|scss)$': 'identity-obj-proxy'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testEnvironment: 'jest-environment-jsdom',
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/api/interfaces/*.ts', '!src/serviceWorker.ts'],
-  moduleDirectories: ['node_modules', 'src']
+    roots: ['<rootDir>/src'],
+    // setupFiles: ["<rootDir>/src/setupTests.ts"],
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.svg$': '<rootDir>/jest.file.js',
+    },
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    ],
+    moduleNameMapper: {
+        '\\.(css|sass|scss)$': 'identity-obj-proxy',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testEnvironment: 'jest-environment-jsdom',
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/api/interfaces/*.ts',
+        '!src/serviceWorker.ts',
+    ],
+    moduleDirectories: ['node_modules', 'src'],
 }
