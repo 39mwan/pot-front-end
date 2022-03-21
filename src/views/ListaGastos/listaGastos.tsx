077e0e10d6ed86a9611cc8d4de672831d60077bf
luismerinou <lmerinoulizarna@gmail.com>
2022-03-21 11:39:11 +0100

    feat: add listaGastos.tsx

---
+++ src/views/ListaGastos/listaGastos.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListaGastos() {
+    return (
+        <div>
+            <h1> Aquí puedes ver los gastos de cada uno</h1>
+        </div>
+    )
+}
+
+export default ListaGastos
