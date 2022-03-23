b916b655de62811831e3ef58304d23a4233b026e
luismerinou <lmerinoulizarna@gmail.com>
2022-03-23 13:27:17 +0100

    feat: add Title component

--- src/components/layout/Main/Body.tsx
+++ src/components/layout/Main/Body.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import ListaGastos from '../../../views/ListaGastos/listaGastos'
 import AnadirAmigo from '../../../views/AnadirAmigo/anadirAmigo'
 import AnadirGasto from '../../../views/AnadirGasto/anadirGasto'
@@ -8,16 +8,42 @@ import Balance from '../../../views/Balance/balance'
 import './Body.css'
 
 function Body() {
+    const location = useLocation()
+
+    function Title() {
+        let pageName
+        switch (location.pathname) {
+            case '/lista-gastos':
+                pageName = 'LISTA DE GASTOS'
+                break
+            case '/amigo':
+                pageName = 'AÑADIR AMIGO/A'
+                break
+            case '/gasto':
+                pageName = 'AÑADIR GASTO'
+                break
+            case '/balance':
+                pageName = 'VER BALANCE'
+                break
+        }
+        return <>{pageName}</>
+    }
+
     return (
         <main>
             <div>
-                <Routes>
-                    <Route path="/" element={<ListaGastos />} />
-                    <Route path="lista-gastos" element={<ListaGastos />} />
-                    <Route path="amigo" element={<AnadirAmigo />} />
-                    <Route path="gasto" element={<AnadirGasto />} />
-                    <Route path="balance" element={<Balance />} />
-                </Routes>
+                <h2>
+                    <Title />
+                </h2>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<ListaGastos />} />
+                        <Route path="lista-gastos" element={<ListaGastos />} />
+                        <Route path="amigo" element={<AnadirAmigo />} />
+                        <Route path="gasto" element={<AnadirGasto />} />
+                        <Route path="balance" element={<Balance />} />
+                    </Routes>
+                </div>
             </div>
         </main>
     )
