0c508ed8a8114f89935ca3aa5206a75a13c14508
39mwan <soniaz1998@gmail.com>
2022-03-25 12:47:55 +0100

    chore: tests routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/layout/navigation/NavBar/NavBarElements'
-import Body from './components/Body'
+import Body from './components/layout/Body/Body'
 import { BrowserRouter } from 'react-router-dom'
 
 function App() {
